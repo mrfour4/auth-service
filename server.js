526d12cc5097ca4d38c6220010b90cc49934655f
mrfour4 <21521609@gm.uit.edu.vn>
2023-06-16 15:49:33 +0700

    clear ui

--- server.js
+++ server.js
@@ -4,9 +4,6 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const cors = require("cors");
-const corsOptions = require("./config/corsOptions");
-const { logger } = require("./middleware/logEvents");
-const errorHandler = require("./middleware/errorHandler");
 const verifyJWT = require("./middleware/verifyJWT");
 const cookieParser = require("cookie-parser");
 const credentials = require("./middleware/credentials");
@@ -25,15 +22,13 @@ connectDB();
 // Connect to Redis
 connectRD();
 
-// custom middleware logger
-app.use(logger);
 
 // Handle options credentials check - before CORS!
 // and fetch cookies credentials requirement
 app.use(credentials);
 
 // Cross Origin Resource Sharing
-app.use(cors(corsOptions));
+app.use(cors());
 
 // built-in middleware to handle urlencoded form data
 app.use(express.urlencoded({ extended: false }));
@@ -45,10 +40,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 //serve static files
-app.use("/", express.static(path.join(__dirname, "/public")));
 
 // routes
-app.use("/", require("./routes/root"));
 app.use("/register", require("./routes/register"));
 app.use("/auth", require("./routes/auth"));
 app.use("/refresh", require("./routes/refresh"));
@@ -60,19 +53,6 @@ app.use(verifyJWT);
 app.use(resHandler);
 
 app.use("/users", require("./routes/api/users"));
-
-app.all("*", (req, res) => {
-    res.status(404);
-    if (req.accepts("html")) {
-        res.sendFile(path.join(__dirname, "views", "404.html"));
-    } else if (req.accepts("json")) {
-        res.json({ error: "404 Not Found" });
-    } else {
-        res.type("txt").send("404 Not Found");
-    }
-});
-
-app.use(errorHandler);
 
 mongoose.connection.once("open", () => {
     console.log("Connected to MongoDB");
